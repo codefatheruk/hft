c2bbae61008986813afcf02ddc6192e96bc3991e
codefatheruk <160322770+codefatheruk@users.noreply.github.com>
2024-02-22 15:19:38 +0000

    Update protfolio_opt.py

--- protfolio_opt.py
+++ protfolio_opt.py
@@ -3,18 +3,18 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 import lightgbm as lgb
 from sklearn.metrics import mean_squared_error
-from scipy.optimize import minimize
 import os
 from glob import glob
 import itertools
+import joblib  # For saving models
+
 os.getcwd()
 os.chdir('/Users/macbookpro/Documents/data')
-file_names=glob('*.zip')
+file_names = glob('*.zip')
 file_names.sort()
-currenies=list(set([i.split('_')[-1].split('.')[0] for i in file_names]))
+currenies = list(set([i.split('_')[-1].split('.')[0] for i in file_names]))
 
-
-file_currency={i:[] for i in currenies}
+file_currency = {i: [] for i in currenies}
 for i in file_names:
     for j in file_currency.keys():
         if j in i:
@@ -38,34 +38,32 @@ for i in file_names:
 file_currency=file_currency_filter
 
 column_names = [['Date']] \
-                  + [[f'BP{i}' , f'BV{i}'] for i in range(1, 6)] \
-                  + ['M'] \
-                  + [[f'AP{i}' , f'AV{i}'] for i in range(1, 6)] \
-                  + [['']]
-column_names=list(itertools.chain.from_iterable(column_names))
+               + [[f'BP{i}', f'BV{i}'] for i in range(1, 6)] \
+               + ['M'] \
+               + [[f'AP{i}', f'AV{i}'] for i in range(1, 6)] \
+               + [['']]
+column_names = list(itertools.chain.from_iterable(column_names))
 
-main_df={i:pd.DataFrame() for i in currenies}
+main_df = {i: pd.DataFrame() for i in currenies}
 for i in main_df.keys():
-    df=pd.DataFrame()
-    print(file_currency[i][31:34])
-    for j in file_currency[i][32:35]:
-        temp_df=pd.read_csv(j,index_col=0,parse_dates=True,names=column_names)
-        temp_df.drop(['M',''],axis=1,inplace=True)
-        temp_df=temp_df.resample('100ms').mean()
-        df=pd.concat([df,temp_df])
-    df.fillna(method='ffill',inplace=True)
-    df.fillna(method='bfill',inplace=True)
-    df['price']=(df['BP1']+df['AP1'])/2
-    df['volume']=(df['BV1']+df['AV1'])/2
-    df['spread']=df['AP1']-df['BP1']
-    df['high']=df['price'].rolling(20).max()
-    df['low']=df['price'].rolling(20).min()
-    df['open']=df['price'].rolling(20).apply(lambda x: x.iloc[0])
-    df['close']=df['price'].rolling(20).apply(lambda x: x.iloc[-1])
-    df.drop(['price'],axis=1,inplace=True)
-    main_df[i]=df
+    df = pd.DataFrame()
+    for j in file_currency[i]:
+        temp_df = pd.read_csv(j, index_col=0, parse_dates=True, names=column_names)
+        temp_df.drop(['M', ''], axis=1, inplace=True)
+        temp_df = temp_df.resample('100ms').mean()
+        df = pd.concat([df, temp_df])
+    df.fillna(method='ffill', inplace=True)
+    df.fillna(method='bfill', inplace=True)
+    df['price'] = (df['BP1'] + df['AP1']) / 2
+    df['volume'] = (df['BV1'] + df['AV1']) / 2
+    df['spread'] = df['AP1'] - df['BP1']
+    df['high'] = df['price'].rolling(20).max()
+    df['low'] = df['price'].rolling(20).min()
+    df['open'] = df['price'].rolling(20).apply(lambda x: x.iloc[0])
+    df['close'] = df['price'].rolling(20).apply(lambda x: x.iloc[-1])
+    df.drop(['price'], axis=1, inplace=True)
+    main_df[i] = df
 
-dates.keys()
 
 lenth={i:main_df[i].dropna().shape[0] for i in main_df.keys()}
 min_length_df = min(lenth, key=lenth.get)
@@ -87,7 +85,42 @@ for i in main_df.keys():
     main_df[i].columns=c_name
 
 window_size=5
-# Generate log return for each dataframe in main_df
+# Generate log return for each dataframe in main_df, categorize it, and split datasets into train and test
+train_test_data = {}
+model_predictions={}
 for currency, df in main_df.items():
     df['log_return'] = np.log(df['close'] / df['close'].shift(window_size))
-    df['log_return'] = df['log_return'].fillna(0)
+    df['log_return_cat'] = df['log_return'].apply(lambda x: 1 if x > 0 else (-1 if x < 0 else 0))
+    df['log_return_cat'] = df['log_return_cat'].fillna(0)
+    X = df.drop(['log_return', 'log_return_cat'], axis=1)
+    y = df['log_return_cat']
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    train_test_data[currency] = {'X_train': X_train, 'X_test': X_test, 'y_train': y_train, 'y_test': y_test}
+    # Train LightGBM model
+    lgb_train = lgb.Dataset(X_train, y_train)
+    lgb_eval = lgb.Dataset(X_test, y_test, reference=lgb_train)
+    params = {
+        'boosting_type': 'gbdt',
+        'objective': 'multiclass',
+        'num_class': 3,
+        'metric': 'multi_logloss',
+        'num_leaves': 31,
+        'learning_rate': 0.05,
+        'feature_fraction': 0.9,
+        'bagging_fraction': 0.8,
+        'bagging_freq': 5,
+        'verbose': 0
+    }
+    gbm = lgb.train(params,
+                    lgb_train,
+                    num_boost_round=20,
+                    valid_sets=lgb_eval,
+                    early_stopping_rounds=5)
+
+    # Save the model
+    joblib.dump(gbm, f'{currency}_lgbm_model.pkl')
+
+    # Make predictions
+    y_pred = gbm.predict(X_test, num_iteration=gbm.best_iteration)
+    model_predictions[currency] = y_pred
+
